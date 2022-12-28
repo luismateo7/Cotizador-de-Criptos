13930fa4a10e1c591956ed00c8168455ac8ecb72
Mateo Rodriguez <luismateor73@gmail.com>
2022-12-28 00:40:26 -0500

    Formulario e imagen con styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
 import { useState } from 'react'
+import Formulario from './components/Formulario'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
 
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  width: 80%;
+  display: block;
+  max-width: 400px;
+  margin: 100px auto 0 auto;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+	color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;  
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Contenedor>
+        <Imagen
+          src={ImagenCripto}
+          alt='Imagen de Criptomonedas'
+        />
+        <div>
+          <Heading>Cotiza Criptomonedas al Instante</Heading>
+          <Formulario />
+        </div>
+      </Contenedor>
     </div>
   )
 }
